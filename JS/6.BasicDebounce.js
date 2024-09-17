ce68a5d8e22134d49ea6fb0b99611c36275052a2
Daniil Karzanov <d.carzanov@gmail.com>
2024-09-17 23:16:15 +0400

    6.BasicDebounce

---
+++ JS/6.BasicDebounce.js
@@ -0,0 +1,10 @@
+// This is a JavaScript coding problem from BFE.dev 
+
+/**
+ * @param {(...args: any[]) => any} func
+ * @param {number} wait
+ * @returns {(...args: any[]) => any}
+ */
+function debounce(func, wait) {
+    // your code here
+  }
